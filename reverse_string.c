1d9b3b5f69e96f035f0971c1dbddc60fc07e5ac5
Jason Tanabe <jasonmt@hawaii.edu>
2015-01-12 00:39:17 -1000

    did reverse string

---
+++ reverse_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  char str[] = "abcd";
+  if (!str) {
+    return;
+  }
+  char* start = str;
+  char* end = str;
+  char tmp;
+  while (*end) {
+    end++;
+  }
+  end--;
+  while (end > start) {
+    tmp = *end;
+    *end = *start;
+    *start = tmp;
+    end--;
+    start++;
+  }
+  printf("%s\n", str);
+}
