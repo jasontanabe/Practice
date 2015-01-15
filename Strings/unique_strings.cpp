98bc6918491cc3745bc9ee11c13198ab05637888
Jason Tanabe <jasonmt@hawaii.edu>
2015-01-14 14:04:34 -1000

    added unique_strings.cpp

---
+++ Strings/unique_strings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string string = "abcdefgb";
+  bool characters[256];
+  for (int i = 0; i < 256; i++) {
+    characters[i] = false;
+  }
+  for (int i = 0; i < string.size(); i++) {
+    if (characters[static_cast<int>(string[i])]) {
+      std::cout << "not unique" << std::endl;
+      return 0;
+    }
+    characters[static_cast<int>(string[i])] = true;
+  }
+  std::cout << "unique" << std::endl;
+}
+
+
